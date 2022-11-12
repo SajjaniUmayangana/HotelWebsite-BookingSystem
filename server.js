39bcf793fb527d06898fabc4be85ecf548d62acc
SajjaniUmayangana <sumayangana2428@gmail.com>
2022-11-12 13:00:45 +0530

    first commit - server

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
-const mongoose = require('mongoose')
+const database = require('./database/connection')
 
 dotenv.config({path:'config.env'})
 
-// CONNECTING TO THE DATABASE ----------------------------------------------------------------
-const mongo_url = process.env.mongo_url
-mongoose.connect(mongo_url)
-
-const connectDB = async() =>{
-    try{
-        // mongodb connection string
-        const con = await mongoose.connect(mongo_url);
-        console.log('MongoDB database connected');
-
-    }catch(err){
-        console.log(err);
-        process.exit(1); // exit from the process (1 = true )
-        }
-}
-////////////////////////////////////////////////////////////////
+// Database connection
+database();
 
 // Middleware ----------------------------------------------------------------
 app.use(express.json())
